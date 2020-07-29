f346db04d42aaef22806b32d35d6dba27cae9d1a
Talent <talentmabuto3@gmail.com>
2020-07-29 15:47:10 +0200

    Django Project

---
+++ cheeses/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.urls import reverse
+from django.conf import settings
+
+from django_countries.fields import CountryField
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+
+class Cheese(TimeStampedModel):
+
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi-soft", "Semi-soft"
+        SEMI_HARD = "semi-hard", "Semi-hard"
+        HARD = "hard", "Hard"
+
+    name = models.CharField("Name of cheese", max_length=200)
+    slug = AutoSlugField("Cheese Address", unique=True,
+        always_update=False, populate_from="name")
+    description = models.TextField("Description", blank=True)
+    firmness = models.CharField("Firmness", max_length=20,
+        choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+    country_of_origin = CountryField("Country of origin", blank=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+        
+    def get_absolute_url(self):
+        return reverse('cheeses:detail', kwargs={'slug': self.slug})
+
+
---
+++ cheeses/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+
+from .views import CheeseListView, CheeseDetailView, CheeseCreateView, CheeseUpdateView
+
+app_name = 'cheeses'
+urlpatterns = [
+    path(route='', view=CheeseListView.as_view(), name='list'),
+    path(route='add/', view=CheeseCreateView.as_view(), name='add'),
+    path(route='<slug:slug>/', view=CheeseDetailView.as_view(), name='detail'),
+    path(route='<slug:slug>/update/', view=CheeseUpdateView.as_view(), name='update'),
+]
